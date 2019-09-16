7ebaffd629e05176efd1ee23c5db0d01949f3bfb
Paolo Pironi <ppiron@gmail.com>
2019-09-16 12:12:22 +0100

    part3.7

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const morgan = require('morgan')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {
