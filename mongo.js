645107baca7425519263a1fc7bb2a41065998a21
Paolo Pironi <ppiron@gmail.com>
2019-09-18 09:01:02 +0100

    part3.11.1 - testing connection to mongodb

---
+++ mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+console.log(password)
+const url =
+  `mongodb+srv://pao:${password}@cluster0-qe630.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4]
+})
+
+person.save().then(response => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})
