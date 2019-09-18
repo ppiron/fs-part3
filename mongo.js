9ed70432e25537abfc6dd9ae9490c72b5336ddbc
Paolo Pironi <ppiron@gmail.com>
2019-09-18 09:42:12 +0100

    part3.12

--- mongo.js
+++ mongo.js
@@ -6,9 +6,9 @@ if (process.argv.length < 3) {
 }
 
 const password = process.argv[2]
-console.log(password)
 const url =
   `mongodb+srv://pao:${password}@cluster0-qe630.mongodb.net/test?retryWrites=true&w=majority`
+// const url = 'mongodb://localhost:27017/myapp'
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -19,12 +19,23 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-  name: process.argv[3],
-  number: process.argv[4]
-})
+if (process.argv.length > 3) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
 
-person.save().then(response => {
-  console.log('person saved!')
-  mongoose.connection.close()
-})
+  person.save().then(response => {
+    console.log(`added ${response.name} number ${response.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+if (process.argv.length === 3) {
+  Person.find().then(items => {
+    const response = items.map(item => `${item.name} ${item.number}`).join('\n')
+    console.log('phonebook:')
+    console.log(response)
+    mongoose.connection.close()
+  })
+}
